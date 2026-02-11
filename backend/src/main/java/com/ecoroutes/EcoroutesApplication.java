88f2577f9db9a607e4866f18ef5e704d97556743
Lorenzo Donato <lorenzodonato75@gmail.com>
2026-02-11 20:27:01 -0300

    chore: initial commit - backend and frontend structure

---
+++ backend/src/main/java/com/ecoroutes/EcoroutesApplication.java
@@ -0,0 +1,13 @@
+package com.ecoroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoroutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoroutesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ecoroutes/EcoroutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoroutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
